4a981376c45abdd270405919c02b2e0ad4a4c952
dawun <dawun@lacy.co.kr>
2024-05-12 17:33:22 +0900

    해피톡 상담 버튼 삽입

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,33 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              var ht = null;
+              (function(id, scriptSrc, callback) {
+                  var d = document,
+                      tagName = 'script',
+                      $script = d.createElement(tagName),
+                      $element = d.getElementsByTagName(tagName)[0];
+          
+                  $script.id = id;
+                  $script.async = true;
+                  $script.src = scriptSrc;
+          
+                  if (callback) { $script.addEventListener('load', function (e) { callback(null, e); }, false); }
+                  $element.parentNode.insertBefore($script, $element);
+              })('happytalkSDK', 'https://chat-static.happytalkio.com/sdk/happytalk.chat.v2.min.js', function() {
+                  ht = new Happytalk({
+                    siteId: '1000425805',
+                    siteName: 'AI Shorts Maker',
+                    categoryId: '180995',
+                    divisionId: '180996'
+                });
+              });
+              `,
+            }}
+          />
         </body>
       </Html>
     );
